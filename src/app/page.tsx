0e87065db0029bbdb81d048d0e6f2d6c48df26c0
LuisCunha05 <luisf.cunha05@gmail.com>
2025-03-04 20:48:36 -0300

    feat: projet config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import React from 'react'
 
-export default function Home() {
+export default function Home(){
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background">
       <h1>Gerenciar de Tabelas 8000</h1>
